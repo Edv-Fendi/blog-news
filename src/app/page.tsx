472d9f6751475936cd4cfaf3eaf4efe977c6a625
Edvaldo Fendi <edvaldo.defendi@gmail.com>
2026-02-18 12:35:44 -0300

    feat: add not found page and improve blog layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,17 @@
 import { PostsList } from "../components/PostsList";
 import { Suspense } from "react";
 import { SpinLoader } from "../components/SpinLoader";
-import { Container } from "../components/Container";
-import { Header } from "../components/Header";
 import { PostFeatured } from "../components/PostFeatured";
 
 export default async function Home() {
   return (
-    <Container>
-      <Header />
+    <>
       <Suspense fallback={<SpinLoader />}>
         <PostFeatured />
       </Suspense>
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
       </Suspense>
-      <footer>
-        <h1 className="text-6xl font-bold text-center py-8">Footer</h1>
-      </footer>
-    </Container>
+    </>
   );
 }
